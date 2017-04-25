f15b30d1c06767e429494446ea74bff9ff6b276c
dongwenlong <dongwenlong@meituan.com>
2017-04-25 20:03:40 +0800

    add thrift test

---
+++ thriftClient/src/main/java/com/meituan/dwl/MyThriftClient.java
@@ -0,0 +1,12 @@
+package com.meituan.dwl;
+
+/**
+ * Created by dongwenlong on 23/04/2017.
+ */
+public class MyThriftClient {
+
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
